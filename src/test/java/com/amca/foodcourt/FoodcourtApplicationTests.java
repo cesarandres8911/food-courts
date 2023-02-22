4681fa6839b85d308a12a2d2aa8c49a61b07fb24
Cesar Meneses <cesarandres8911@gmail.com>
2023-02-21 20:08:21 -0500

    Initial release

---
+++ src/test/java/com/amca/foodcourt/FoodcourtApplicationTests.java
@@ -0,0 +1,14 @@
+package com.amca.foodcourt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodcourtApplicationTests {
+
+	@SuppressWarnings("squid:S2699")
+	@Test
+	void contextLoads() {
+	}
+
+}
